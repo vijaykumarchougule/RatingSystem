086954e9665bfc4f8bf3ae3a35840d43bb321f18
Vijaykumar <vijaykumarchougule2001@gmail.com>
2023-06-24 01:19:01 +0530

    Project Created for Rating System -VJ

---
+++ src/main/java/com/ratingsystem/entities/Rating.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.ratingsystem.entities;
+
+/**
+ * @author vijay
+ */
+
+public class Rating {
+
+
+	private String ratingId;
+	
+	private String userId;
+	
+	private String teacherId;
+	
+	private int rating;
+	
+	private String feedback;
+}
